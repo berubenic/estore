1542d246744a0dbb32e44ac216c9da64a00bb948
berubenic <berubenic@gmail.com>
2021-07-31 15:31:00 -0400

    changed logo through spree.rb config file, needs a *.svg

--- config/initializers/spree.rb
+++ config/initializers/spree.rb
@@ -33,7 +33,7 @@ Spree.config do |config|
   # Frontend:
 
   # Custom logo for the frontend
-  # config.logo = "logo/solidus.svg"
+  config.logo = "logo/banana_logo.svg"
 
   # Template to use when rendering layout
   # config.layout = "spree/layouts/spree_application"
