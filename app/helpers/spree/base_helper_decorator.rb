b9619f92de63676b58e51c5d29e70693de017b47
berubenic <berubenic@gmail.com>
2021-08-06 15:39:47 -0400

    sidebar that collapses with rotating arrow

--- app/helpers/spree/base_helper_decorator.rb
+++ app/helpers/spree/base_helper_decorator.rb
@@ -30,5 +30,20 @@ module Spree
 
     link_to text.html_safe, spree.cart_path, class: "nav-link cart-info #{css_class}"
   end
+
+
+  def taxons_tree(root_taxon, current_taxon, max_level = 1)
+    return '' if max_level < 1 || root_taxon.children.empty?
+    content_tag :ul, class: 'taxons-list btn-toggle-nav list-unstyled fw-normal pb-1 small' do
+      taxons = root_taxon.children.map do |taxon|
+        css_class = (current_taxon && current_taxon.self_and_ancestors.include?(taxon)) ? 'current' : nil
+        content_tag :li, class: css_class do
+         link_to(taxon.name, seo_url(taxon)) +
+           taxons_tree(taxon, current_taxon, max_level - 1)
+        end
+      end
+      safe_join(taxons, "\n")
+    end
+  end
 end
 end
